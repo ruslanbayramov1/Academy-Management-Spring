f4baf2ea7effe674da8c8382244dc2015149ac25
ballas01 <rus.bayramov.05@inbox.ru>
2025-05-10 22:05:40 +0400

    Implementing full CRUD for student

--- src/main/java/org/springframework/academymanagement/service/StudentService.java
+++ src/main/java/org/springframework/academymanagement/service/StudentService.java
@@ -1,6 +1,8 @@
 package org.springframework.academymanagement.service;
 
+import org.springframework.academymanagement.dto.StudentCreateDTO;
 import org.springframework.academymanagement.dto.StudentGetDTO;
+import org.springframework.academymanagement.dto.StudentUpdateDTO;
 import org.springframework.academymanagement.repository.StudentRepository;
 import org.springframework.academymanagement.entity.Student;
 import org.springframework.academymanagement.mapper.StudentMapper;
@@ -8,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class StudentService {
@@ -26,13 +29,29 @@ public class StudentService {
     }
 
     public StudentGetDTO findByEmail(String email) {
-        Student student = studentRepository.findByEmail(email).orElseThrow(() -> new RuntimeException(""));
+        Student student = studentRepository.findByEmail(email).orElseThrow(() -> new RuntimeException("Student not found."));
         return studentMapper.studentToStudentGetDTO(student);
     }
 
     public StudentGetDTO findByStudentCode(String studentCode) {
-        Student student = studentRepository.findByStudentCode(studentCode).orElseThrow(() -> new RuntimeException(""));
+        Student student = studentRepository.findByStudentCode(studentCode).orElseThrow(() -> new RuntimeException("Student not found."));
         return studentMapper.studentToStudentGetDTO(student);
+    }
+
+    public void createStudent(StudentCreateDTO studentCreateDTO) {
+        Student student = studentMapper.studentCreateDTOToStudent(studentCreateDTO);
+        studentRepository.save(student);
+    }
+
+    public void updateStudent(UUID id, StudentUpdateDTO studentUpdateDTO) {
+        Student student = studentRepository.findById(id).orElseThrow(() -> new RuntimeException("Student not found."));
+        studentMapper.updateStudentFromDto(studentUpdateDTO, student);
+        studentRepository.save(student);
+    }
+
+    public void deleteStudent(UUID id) {
+        Student student = studentRepository.findById(id).orElseThrow(() -> new RuntimeException("Student not found."));
+        studentRepository.deleteById(id);
     }
 
     public Student saveStudent(Student student) {
