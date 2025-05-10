de7eac3dac207bfe56a360272c4763841602eb3e
ballas01 <rus.bayramov.05@inbox.ru>
2025-05-10 22:05:51 +0400

    Implementing full CRUD mapping

--- src/main/java/org/springframework/academymanagement/mapper/StudentMapper.java
+++ src/main/java/org/springframework/academymanagement/mapper/StudentMapper.java
@@ -2,6 +2,9 @@ package org.springframework.academymanagement.mapper;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+import org.springframework.academymanagement.dto.StudentCreateDTO;
+import org.springframework.academymanagement.dto.StudentUpdateDTO;
 import org.springframework.academymanagement.entity.Student;
 import org.springframework.academymanagement.dto.StudentGetDTO;
 
@@ -9,4 +12,10 @@ import org.springframework.academymanagement.dto.StudentGetDTO;
 public interface StudentMapper {
 
     StudentGetDTO studentToStudentGetDTO(Student student);
+
+    @Mapping(target = "id", ignore = true)
+    Student studentCreateDTOToStudent(StudentCreateDTO studentCreateDTO);
+
+    @Mapping(target = "id", ignore = true)
+    void updateStudentFromDto(StudentUpdateDTO studentUpdateDTO, @MappingTarget Student student);
 }
