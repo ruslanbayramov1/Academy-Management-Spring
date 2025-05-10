0d292967121581241f269b6371b5aba2d1b8071d
ballas01 <rus.bayramov.05@inbox.ru>
2025-05-10 22:06:15 +0400

    Implementing full CRUD and http response

--- src/main/java/org/springframework/academymanagement/controller/StudentController.java
+++ src/main/java/org/springframework/academymanagement/controller/StudentController.java
@@ -1,12 +1,16 @@
 package org.springframework.academymanagement.controller;
 
+import jakarta.validation.Valid;
+import org.springframework.academymanagement.dto.StudentCreateDTO;
 import org.springframework.academymanagement.dto.StudentGetDTO;
+import org.springframework.academymanagement.dto.StudentUpdateDTO;
 import org.springframework.academymanagement.service.StudentService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("api/v1/students")
@@ -18,12 +22,25 @@ public class StudentController {
     }
 
     @GetMapping
-    public List<StudentGetDTO> getStudents() {
-        return studentService.getAllStudents();
+    public ResponseEntity<List<StudentGetDTO>> getStudents() {
+        return ResponseEntity.ok(studentService.getAllStudents());
     }
 
-    @GetMapping("/hi")
-    public String getText() {
-        return "Hi! U finally solved the problem after 4 hours :)";
+    @PostMapping
+    public ResponseEntity<String> createStudent(@RequestBody @Valid StudentCreateDTO student) {
+        studentService.createStudent(student);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @PutMapping
+    public ResponseEntity<String> updateStudent(@RequestHeader UUID id, @RequestBody @Valid StudentUpdateDTO student) {
+        studentService.updateStudent(id, student);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @DeleteMapping
+    public ResponseEntity<String> deleteStudent(@RequestHeader UUID id) {
+        studentService.deleteStudent(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 }
