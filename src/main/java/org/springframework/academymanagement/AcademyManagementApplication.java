ec857846a66cbb45285d0d046b2a8549494590e3
ballas01 <rus.bayramov.05@inbox.ru>
2025-05-10 11:17:56 +0400

    Initial

---
+++ src/main/java/org/springframework/academymanagement/AcademyManagementApplication.java
@@ -0,0 +1,13 @@
+package org.springframework.academymanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademyManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AcademyManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/springframework/academymanagement/AcademyManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package org.springframework.academymanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademyManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
