0c4ca843a846a8343c684eb0f00919c83ec1673d
ballas01 <rus.bayramov.05@inbox.ru>
2025-05-10 16:00:08 +0400

    Preparing environment

--- src/main/java/org/springframework/academymanagement/AcademyManagementApplication.java
+++ src/main/java/org/springframework/academymanagement/AcademyManagementApplication.java
@@ -3,6 +3,7 @@ package org.springframework.academymanagement;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class AcademyManagementApplication {
 
