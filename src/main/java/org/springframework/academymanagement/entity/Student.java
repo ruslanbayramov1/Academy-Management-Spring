938d5f0f9b8157ba538c71b8ed1e25ace685fb7b
ballas01 <rus.bayramov.05@inbox.ru>
2025-05-10 12:17:38 +0400

    student entity

---
+++ src/main/java/org/springframework/academymanagement/entity/Student.java
@@ -0,0 +1,40 @@
+package org.springframework.academymanagement.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Table(name = "students")
+@Getter @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Student {
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @Column(nullable = false, length = 64)
+    private String firstName;
+
+    @Column(nullable = false, length = 64)
+    private String lastName;
+
+    @Column(nullable = false, length = 312, unique = true)
+    private String email;
+
+    @Column(nullable = true)
+    private LocalDate birthDate;
+
+    @Column(nullable = false, length = 64)
+    private String gender;
+
+    @Column(nullable = false, length = 64, unique = true)
+    private String studentCode;
+
+    @Column(nullable = false, length = 64, unique = true)
+    private String groupCode;
+}
