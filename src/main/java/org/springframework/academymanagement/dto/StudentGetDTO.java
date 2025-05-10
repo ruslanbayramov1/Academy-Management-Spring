184ddacedba666041a434d7ed1004622e9571bb3
ballas01 <rus.bayramov.05@inbox.ru>
2025-05-10 15:59:05 +0400

    Initiating Student dto

---
+++ src/main/java/org/springframework/academymanagement/dto/StudentGetDTO.java
@@ -0,0 +1,15 @@
+package org.springframework.academymanagement.dto;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+public record StudentGetDTO(
+        UUID id,
+        String firstName,
+        String lastName,
+        String email,
+        LocalDate birthDate,
+        String gender,
+        String studentCode,
+        String groupCode
+) { }
