f061dee29f6c38230cb3f13c8ac59964fe6128e3
ballas01 <rus.bayramov.05@inbox.ru>
2025-05-15 15:34:29 +0400

    Adding exceptions cases for field errors and general errors

--- src/main/java/org/springframework/academymanagement/handler/GlobalExceptionHandler.java
+++ src/main/java/org/springframework/academymanagement/handler/GlobalExceptionHandler.java
@@ -5,8 +5,12 @@ import org.springframework.academymanagement.dto.response.ExceptionResponse;
 import org.springframework.academymanagement.exception.BaseRuntimeException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -14,6 +18,27 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(BaseRuntimeException.class)
     public ResponseEntity<ExceptionResponse<Void>> handleBaseException(BaseRuntimeException ex) {
         HttpStatus status = ex.getStatus();
+        String message = ex.getMessage();
+
+        ExceptionResponse<Void> response = new ExceptionResponse<>(false, status, message);
+        return ResponseEntity.status(response.getStatus()).body(response);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ExceptionResponse<Map<String, String>>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult().getFieldErrors().forEach(error -> {
+            errors.put(error.getField(), error.getDefaultMessage());
+        });
+
+        ExceptionResponse<Map<String, String>> response = new ExceptionResponse<>(false, HttpStatus.BAD_REQUEST, "Validation failed" , errors);
+        return ResponseEntity.badRequest().body(response);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ExceptionResponse<Void>> handleBaseException(Exception ex) {
+        HttpStatus status = HttpStatus.BAD_REQUEST;
         String message = ex.getMessage();
 
         ExceptionResponse<Void> response = new ExceptionResponse<>(false, status, message);
